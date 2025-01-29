6f2c5ebebf6e713bd6ffccd3b2424b5a451137d2
glors8 <gloria.dabre@outlook.com>
2025-01-29 09:37:42 -0600

    se agrega gráfico de dispersión falante

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ if hist_button: # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
 
 
+scatter_button = st.button('Construir gráfico de dispersión')
 
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data,x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True) # crear un botón
 
